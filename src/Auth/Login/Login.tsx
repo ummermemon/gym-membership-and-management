666779dd924b12a50e8be27d690bf830320cf8c4
Ummer Memon <umemon0001@gmail.com>
2025-12-28 16:05:53 +0530

    tailwind css added

---
+++ src/Auth/Login/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div className='bg-amber-900'>LoginPage</div>
+  )
+}
+
+export default LoginPage
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginPage from './Auth/Login/Login.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <LoginPage />
   </StrictMode>,
 )
