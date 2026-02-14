42966225f22ec0b1c014fa94544ea042fd9dbc2d
Ummer Memon <umemon0001@gmail.com>
2026-02-14 22:41:29 +0530

    edit profile added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LandingPage from "./pages/frontend/LandingPage.tsx";
 import DashboardPage from "./pages/member/DasboardPage.tsx";
 import PublicRoute from "./routes/PublicRoute.tsx";
 import ProtectedRoute from "./routes/ProtectedRoute.tsx";
+import EditProfilePage from "./pages/member/profile/EditProfilePage.tsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       
       <Route path="/login" element={<PublicRoute><LoginPage /></PublicRoute>} />
       <Route path="/member/dashboard" element={<ProtectedRoute><DashboardPage /></ProtectedRoute>} />
+      <Route path="/member/edit-profile" element={<ProtectedRoute><EditProfilePage /></ProtectedRoute>} />
     </Routes>
   );
 }
--- src/pages/member/components/Header.tsx
+++ src/pages/member/components/Header.tsx
@@ -15,10 +15,10 @@ import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 function Header() {
-    const navigate = useNavigate();
-    const [profileOpen, setProfileOpen] = useState(false);
-    const [user, setUser] = useState(null);
-    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  const navigate = useNavigate();
+  const [profileOpen, setProfileOpen] = useState(false);
+  const [user, setUser] = useState(null);
+  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
   useEffect(() => {
     fetchUser();
@@ -31,10 +31,10 @@ function Header() {
       const response = await fetch(
         `${API_BASE_URL}/api/me`,
         {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
       );
       const res = await response.json();
@@ -44,47 +44,47 @@ function Header() {
     }
   };
 
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        navigate("/login");
-    };
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
 
-    return (
-        <>
-            <header className="bg-white shadow-sm p-4 flex justify-between items-center">
-                <h2 className="text-xl font-semibold">Dashboard</h2>
+  return (
+    <>
+      <header className="bg-white shadow-sm p-4 flex justify-between items-center">
+        <h2 className="text-xl font-semibold">Dashboard</h2>
 
-                <div className="relative">
-                    <button
-                        onClick={() => setProfileOpen(!profileOpen)}
-                        className="flex items-center gap-2"
-                    >
-                        <img src={
-                          user?.profile_img
-                            ? `${API_BASE_URL}/storage/users/profile_images/${user.profile_img}`
-                            : "/profile.png" 
-                        } className="h-10"></img>
-                        <div className="w-9 h-9 bg-blue-500 text-white rounded-full flex items-center justify-center">
-                            U
-                        </div>
-                        <span className="hidden md:block font-medium"> {user?.fname || "Member"}</span>
-                    </button>
+        <div className="relative">
+          <button
+            onClick={() => setProfileOpen(!profileOpen)}
+            className="flex items-center gap-2"
+          >
+            <img src={
+              user?.profile_img
+                ? `${API_BASE_URL}/storage/users/profile_images/${user.profile_img}`
+                : "/profile.png"
+            } className="h-10"></img>
+            <span className="hidden md:block font-medium"> {user?.fname || "Member"}</span>
+          </button>
 
-                    {profileOpen && (
-                        <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-md border">
-                            <DropdownItem icon={<User size={16} />} label="Edit Profile" />
-                            <DropdownItem icon={<KeyRound size={16} />} label="Change Password" />
-                            <DropdownItem
-                            icon={<LogOut size={16} />}
-                            label="Logout"
-                            onClick={handleLogout}
-                            />
-                        </div>
-                    )}
-                </div>
-            </header>
-        </>
-    )
+          {profileOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white rounded-lg shadow-md border">
+              <DropdownItem onClick={() => {
+                setProfileOpen(false);
+                navigate("/member/edit-profile");
+              }} icon={<User size={16} />} label="Edit Profile" />
+              <DropdownItem icon={<KeyRound size={16} />} label="Change Password" />
+              <DropdownItem
+                icon={<LogOut size={16} />}
+                label="Logout"
+                onClick={handleLogout}
+              />
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  )
 }
 const DropdownItem = ({ icon, label, onClick }) => (
   <div onClick={onClick} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 cursor-pointer text-sm">
