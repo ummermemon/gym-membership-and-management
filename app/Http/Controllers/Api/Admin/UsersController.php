35dd5e01d85932eb8a60ace1cbfc64a89c1e68b5
Ummer Memon <umemon0001@gmail.com>
2026-02-26 21:31:30 +0530

    returning view user with memberships

--- app/Http/Controllers/Api/Admin/UsersController.php
+++ app/Http/Controllers/Api/Admin/UsersController.php
@@ -58,7 +58,7 @@ class UsersController extends Controller
     }
     public function view(Request $request, $id)
     {
-        $user = User::with('activeMembership')->where('id', $id)->get();
+        $user = User::with(['activeMembership', 'memberships'])->where('id', $id)->get();
         return response()->json([
             'status' => true,
             'message' => 'User Fetch Successfully',
